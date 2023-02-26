1bafedf2cd7ceadf73a6dacba33d4a5f7ce72f81
JorgeLuis1900 <jorgekik37@gmail.com>
2023-02-25 23:23:12 -0600

    25/02/23 - trabajoenclase

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
+from flask import Flask,render_template,redirect,url_for,request, make_response, flash
+import forms
+from flask_wtf.csrf import CSRFProtect
 
-#Referenciamos Flask
-from flask import Flask, render_template
-from flask import request
-import forms 
-import numeroForm 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tu_clave_secreta_aqui'
+csrf = CSRFProtect(app)
 
-app= Flask(__name__) 
+@app.errorhandler(404)
+def no_encontrada(e):
+    return render_template('404.html'),404
+
+@app.route("/cookies", methods = ['GET','POST'])
+def cookies():
+    reg_user = forms.LoginForm(request.form)
+    datos = ''
+    if request.method == 'POST' and reg_user.validate():
+        user = reg_user.username.data
+        passw = reg_user.password.data
+        datos = user + '@' + passw
+        success_message = 'Bienvenido {}'.format(user)
+        flash(success_message)
+    
+    response = make_response(render_template('cookies.html', form = reg_user))
+    if len(datos) > 0 :
+        response.set_cookie('datos_user', datos)
+    
+    return response
+
+@app.route("/saludo")
+def saludo():
+    valor_cookie = request.cookies.get('datos_user')
+    nombres = valor_cookie.split('@')
+    return render_template('saludo.html', nom = nombres[0])
+
 
 #Rutas de paginas decorador
 @app.route("/formulario")
@@ -13,20 +40,17 @@ def formulario():
     return render_template("formulario.html") 
 
 @app.route("/Alumnos",methods=['GET','POST'])
-def alumnos():
-    alum_form= forms.UseForm(request.form)
-
-    if request.method=='POST':
+def alumnos():    
+    alum_form = forms.UserForm(request.form)
+    if request.method == 'POST' and alum_form.validate() :
         print(alum_form.matricula.data)
-        print(alum_form.nombre.data)
-
-
-    return render_template("Alumnos.html",form=alum_form) 
+        print(alum_form.nombre.data)             
+    return render_template('alumnos.html',form=alum_form)
 
 @app.route("/cajasDinamicas",methods=['GET','POST'])
 def cajasDinamicas():
     numero=0
-    alum_form= numeroForm.UseForm(request.form)
+    alum_form= forms.UseFormulario(request.form)
     if request.method=='POST':
         numero= request.form.get("numero")
     return render_template("cajasDinamicas.html",form=alum_form,cajas=int(numero)) 
--- forms.py
+++ forms.py
@@ -2,8 +2,41 @@ from wtforms import Form
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,FieldList,FormField,SelectField,RadioField
 from wtforms.fields import EmailField
-class UseForm(Form):
+from wtforms import IntegerField
+from wtforms import validators
+class UseFormulario(Form):
+    #alumnos
     matricula=StringField("Matricula")
     nombre=StringField("Nombre")
     apaterno=StringField("Apaterno")
     email=EmailField("Correo")
+
+    #Cajas Dinamicas
+    numero=StringField("Numero")
+    numeroDinamicos=StringField("Numero")
+
+class LoginForm(Form):
+    username=StringField('usuario',[
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4,max=15,message='No cumple con la longitud para el campo')])
+    password=StringField('password',[
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4,max=15,message='No cumple con la longitud para el campo')])
+    
+def mi_Validacion(form, field):
+    if len(field.data) == 0:
+        raise validators.ValidationError('El campo no tiene datos')
+
+class UserForm(Form):
+    matricula = StringField('Matricula',[
+                            validators.DataRequired(message='El campo es requerido'),
+                            validators.length(min=4, max=10, message='long de campo 4 min and 5 max')])
+    nombre = StringField('Nombre',[
+                         validators.DataRequired(message='El campo es requerido')])
+    apaterno = StringField('Apaterno', [mi_Validacion])
+    amaterno = StringField('Amaterno')
+    email = EmailField('Email')
+
+
+class DynamicForm(FlaskForm):
+    pass
